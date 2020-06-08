3c40e3f06da6ad41a6a40a836bf6837b2f48eae7
Sven Põder <Sven.Poder@LIVE.COM>
2020-06-08 18:45:32 +0300

    Added new test table, first implementation of velocity field plotter

--- gaia-tools/data_analysis.py
+++ gaia-tools/data_analysis.py
@@ -2,6 +2,7 @@
 
 import pandas as pd
 import numpy as np
+from scipy import stats
 import astropy
 import astropy.coordinates as coord
 from astropy.coordinates import SkyCoord
@@ -77,6 +78,77 @@ def transform_to_galcen(df, z_sun=17*u.pc, galcen_distance=8.178*u.kpc):
 
     return c
 
+# TODO: Implement binning in all projections -> x, y, z. (Currently only on x-y plane)
+'''
+Function for binning data in 2 dimensions. Used to plot vector maps of velocities.
+Input parameters:
+    BL - Bin Limit (The edges of the xyz boundary)
+'''
+def bin_data(galcen_data, show_bins = False, BL = 20000):
+   
+    
+
+    # Map values to temporary data frame.
+    plottable_df = pd.DataFrame({'x': galcen_data.x.value,
+                            'y':galcen_data.y.value,
+                            'z':galcen_data.z.value,
+                            'v_x':galcen_data.v_x.value,
+                            'v_y':galcen_data.v_y.value,
+                            'v_z':galcen_data.v_z.value})
+
+    # Define spatial limits.
+    plottable_df = plottable_df[(plottable_df.x >= -BL) & (plottable_df.x <= BL)]
+    plottable_df = plottable_df[(plottable_df.y >= -BL) & (plottable_df.y <= BL)]
+    plottable_df = plottable_df[(plottable_df.z >= -BL) & (plottable_df.z <= BL)]
+
+    # x, y coordinates of the points
+    x = -plottable_df.x
+    y = plottable_df.y
+
+    # Velocity projections of points
+    z = -plottable_df.v_x
+    z2 = plottable_df.v_y
+
+    #v_x
+    H, xedges, yedges, binnumber = stats.binned_statistic_2d(x, y, values = z,bins = 10, statistic='mean')
+
+    #v_y
+    H2, xedges2, yedges2, binnumber2 = stats.binned_statistic_2d(x, y, values = z2,bins = 10, statistic='mean')
+
+    if(show_bins == True):
+        from data_plot import display_bins
+
+        display_bins(xedges, yedges, {'Data': H, 'Projection':'x', 'Unit':galcen_data.v_x.unit})
+        display_bins(xedges, yedges, {'Data': H2, 'Projection':'y', 'Unit':galcen_data.v_y.unit})
+    
+    VEC_COORDS = generate_vector_mesh(xedges, yedges)
+
+    return ({'Data':[H, H2], 
+            'Projections':['x', 'y'],
+            'Velocity Units':[galcen_data.v_x.unit, galcen_data.v_y.unit],
+            'Distance Units':[galcen_data.x.unit, galcen_data.y.unit],
+            'Vector Coordinates':VEC_COORDS})
+
+'''
+Function for finding center points of bins in binned data and then 
+creating a meshgrid out of all the point coordinates. The center points of bins
+are the origin points for the vectors.
+'''
+def generate_vector_mesh(xedges, yedges):
+    vec_x = []
+    vec_y = []
+    vec_z = []
+
+    # TODO: Automate this for n
+    for i in range(10):
+        vec_x.append((xedges[i+1]+xedges[i])/2)
+        vec_y.append((yedges[i+1]+yedges[i])/2)
+
+    # We create a meshgrid out of all the vector locations
+    VEC_XX, VEC_YY = np.meshgrid(vec_x, vec_y)
+
+    return VEC_XX, VEC_YY
+
 def main():
     
     # For finding current module working directory
@@ -93,9 +165,8 @@ def main():
     #job = Gaia.launch_job(query=query, dump_to_file = True, output_format='csv', output_file='user_uploaded_table_test.csv')
 
     #print(job)
-    # Get first 10 non-header rows from our data file
     
-    my_path = 'user_uploaded_table_test.csv'
+    my_path = 'spectroscopic_test_table.csv'
     df = pd.read_csv(my_path)
    
     print("The dimensions of the data: (rows, columns) -> {}".format(df.shape))
@@ -115,10 +186,13 @@ def main():
     print("Transforming data to galactocentric frame...")
     galcen = transform_to_galcen(df)
 
-    from data_plot import distribution_hist, point_density
+    from data_plot import distribution_hist, point_density, display_bins, generate_velocity_map
     distribution_hist(galcen)
    
-    point_density(galcen, 2000)
+    #point_density(galcen, 2000)
+
+    bin_dict = bin_data(galcen, show_bins = True)
+    generate_velocity_map(bin_dict)
 
     print("Plotting done!")
 
--- gaia-tools/data_plot.py
+++ gaia-tools/data_plot.py
@@ -1,6 +1,8 @@
 from matplotlib.ticker import FormatStrFormatter
 from matplotlib.ticker import StrMethodFormatter
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import matplotlib.cm as cm
 import numpy as np
 import mpl_scatter_density
 from astropy.visualization import LogStretch
@@ -65,6 +67,77 @@ def point_density(galcen, vmax):
 
     plt.show()
     
+
+'''
+A plot which enables the user to see the bins created by the 'bin_data' functions in the 
+data analysis module. It takes in the histogram data and does a colormesh with colours 
+mapped to the mean value inside the 2D bin.
+'''
+def display_bins(xedges, yedges, data_dict):
+
+    XX, YY = np.meshgrid(xedges, yedges)
+
+    fig = plt.figure(figsize = (7,7))
+    ax1=plt.subplot(111)
+    plot1 = ax1.pcolormesh(XX,YY,data_dict['Data'].T)
+
+    cbar = plt.colorbar(plot1,ax=ax1, pad = .015, aspect=10, label='2D Bins Velocity V{0}[{1}]'.format(data_dict['Projection'], 
+                                                                                                                   data_dict['Unit']))
+
+    plt.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
+    plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+
+    ax1.set_xlabel('$x$ [{0:latex_inline}]'.format(data_dict['Unit']), fontdict={'fontsize': 18})
+    ax1.set_ylabel('$y$ [{0:latex_inline}]'.format(data_dict['Unit']), fontdict={'fontsize': 18})
+
+    plt.title("2D Bins Velocity V{0}".format(data_dict['Projection']), pad=20, fontdict={'fontsize': 20})
+    
+    plt.show()
+
+
+'''
+Generates a velocity field from binned data.
+Input parameters:
+    binned_dict - A dictionary containing all requisite data for displaying the vector field
+'''
+def generate_velocity_map(binned_dict):
+ 
+    H = binned_dict['Data'][0]
+    H2 = binned_dict['Data'][1]
+
+    VEC_XX, VEC_YY = binned_dict['Vector Coordinates']
+
+    fig, ax = plt.subplots(figsize = (7,7))
+
+
+    # Gives the hypotenuse of vectors
+    M = np.hypot(H.T, H2.T)
+
+    norm = mpl.colors.Normalize()
+    norm.autoscale(M)
+    cm = mpl.cm.jet
+
+    sm = mpl.cm.ScalarMappable(cmap=cm, norm=norm)
+    sm.set_array([])
+
+    # The quiver plot with normalised vector lengths
+    q = ax.quiver(VEC_XX, VEC_YY, H.T, H2.T, M, cmap=plt.cm.jet)
+
+    plt.colorbar(sm)
+
+    # Formats x-y axis in scientific notation
+    plt.ticklabel_format(axis="x", style="sci", scilimits=(0,0))
+    plt.ticklabel_format(axis="y", style="sci", scilimits=(0,0))
+
+    # TODO: Implement for arbitrary limits
+    plt.xlim(-20000,20000)
+    plt.ylim(-20000,20000)
+
+    ax.set_xlabel('$x$ [{0:latex_inline}]'.format(binned_dict['Distance Units'][0]), fontdict={'fontsize': 18})
+    ax.set_ylabel('$y$ [{0:latex_inline}]'.format(binned_dict['Distance Units'][1]), fontdict={'fontsize': 18})
+    
+    plt.grid()
+    plt.show()
 
 def main():
     print("I am the main function")
