b290e1e0fd6e7e4eb887310efca5a1bed52c49c3
Sven Põder <Sven.Poder@LIVE.COM>
2021-03-08 18:59:15 +0200

    Implemented mcmc class, fixed run_sampler function

--- gaia_tools/mcmc.py
+++ gaia_tools/mcmc.py
@@ -1,110 +1,122 @@
 '''
-Module containing neccessary functions for MCMC loop.
+Class containing neccessary functions for MCMC loop.
 '''
 from . import data_analysis
 from . import covariance_generation as cov
 import numpy as np
 import emcee
 
-# LogLikelihood for all the bins
-# If so will pass in arguments transformed Bin object
+class MCMCLooper:
 
-def log_likelihood(theta, data_icrs):
+    def __init__(self, icrs_data, theta_0, debug = False):
+
+        self.icrs_data = icrs_data
+        self.theta_0 = theta_0
+        self.debug = debug
+
+   
+
+    def log_likelihood(theta):
 
 
-    # Example likelihood from emcee documentation
-    #m, b, log_f = theta
-    #model = m * x + b
-    #sigma2 = yerr ** 2 + model ** 2 * np.exp(2 * log_f)
-    #return -0.5 * np.sum((y - model) ** 2 / sigma2 + np.log(sigma2))
-    # -----------------------------------------------
+        # Example likelihood from emcee documentation
+        #m, b, log_f = theta
+        #model = m * x + b
+        #sigma2 = yerr ** 2 + model ** 2 * np.exp(2 * log_f)
+        #return -0.5 * np.sum((y - model) ** 2 / sigma2 + np.log(sigma2))
+        # -----------------------------------------------
 
-    #region Transform Data
+        #region Transform Data
 
-    '''
-    Transfrom from ICRS to Cylindrical galactocentric coordinates 
+        '''
+        Transfrom from ICRS to Cylindrical galactocentric coordinates 
 
-    '''
+        '''
     
-    galcen_data = data_analysis.get_transformed_data(data_icrs, 
-                                                     include_cylindrical = True, 
-                                                     z_0 = theta.z,
-                                                     r_0 = theta.r,
-                                                     v_sun = theta.v_sun,
-                                                     debug = True, 
-                                                     is_source_included = True)
+        galcen_data = data_analysis.get_transformed_data(self.data_icrs, 
+                                                         include_cylindrical = True, 
+                                                         z_0 = theta.z,
+                                                         r_0 = theta.r,
+                                                         v_sun = theta.v_sun,
+                                                         debug = True, 
+                                                         is_source_included = True)
     
-    cov_dict = cov.generate_covmatrices(df = data_icrs, 
-                                        df_crt = galcen, 
-                                        transform_to_galcen = False, 
-                                        transform_to_cylindrical = True,
-                                        z_0 = theta.z,
-                                        r_0 = theta.r,
-                                        debug=True)
+        cov_df = cov.generate_covmatrices(df = self.data_icrs, 
+                                            df_crt = galcen_data, 
+                                            transform_to_galcen = False, 
+                                            transform_to_cylindrical = True,
+                                            z_0 = theta.z,
+                                            r_0 = theta.r,
+                                            debug=True)
 
-    #endregion 
+        # Append covariance information to galactocentric data
+        galcen_data['cov_mat'] = cov_df['cov_mat']
 
-    #region Bin Data
+        #endregion 
 
-    # Declared variable with BinCollection object
-    bins = data_analysis.get_collapsed_bins(galcen_data, BL_r = 100000, BL_z = 5000, N_bins = (10, 10))
+        #region Bin Data
 
-    # Populates bins with their MLE values of mean and variance
-    bins.GetMLEParameters()
+        # Declared variable with BinCollection object
+        bins = data_analysis.get_collapsed_bins(galcen_data, BL_r = 100000, BL_z = 5000, N_bins = (10, 10))
 
-    #endregion
+        # Populates bins with their MLE values of mean and variance
+        bins.GetMLEParameters()
 
-    #region Calculate Likelihoods
+        #endregion
+
+        #region Calculate Likelihoods
     
-    likelihood_array = np.zeros(bins.N_bins)
+        likelihood_array = np.zeros(bins.N_bins)
 
-    # Now we need to calculate likelihood values for each bin
-    for i, bin in enumerate(bins):
+        # Now we need to calculate likelihood values for each bin
+        for i, bin in enumerate(bins):
 
-        # Get Bin likelihood
-        likelihood_value = bin.get_bin_likelihood()
+            # Get Bin likelihood
+            likelihood_value = bin.get_bin_likelihood()
         
-        # Add to array
-        likelihood_array[i] = likelihood_value
+            # Add to array
+            likelihood_array[i] = likelihood_value
     
-    # Square sum likelihoods over all bins
-    likelihood_sum = np.sum(likelihood_array**2)
+        # Square sum likelihoods over all bins
+        likelihood_sum = np.sum(likelihood_array**2)
 
-    #endregion
+        #endregion
 
-    return likelihood_sum
+        return likelihood_sum
 
-def log_prior(theta):
-
-
-    ## Prior assumptions of our parameters
-    ## Flat across all parameters at first
-
-    return -np.inf
+    def log_prior(theta):
 
 
-def log_probability(theta, data_icrs):
+        ## Prior assumptions of our parameters
+        ## Flat across all parameters at first
 
-    lp = log_prior(theta)
-
-    if not np.isfinite(lp):
         return -np.inf
 
-    return lp + log_likelihood(theta, data_icrs)
 
+    def log_probability(theta):
 
-def run_sampler(THETA_0):
+        lp = log_prior(theta)
 
-    # Our initial starting point for the sampler
-    # Fix this
-    pos = THETA_0
+        if not np.isfinite(lp):
+            return -np.inf
 
-    nwalkers = 32
-    ndim = 5
+        return lp + log_likelihood(theta)
 
-    sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability)
-    sampler.run_mcmc(pos, 5000, progress=True);
+    # TODO: Configure burn in steps!
+    def run_sampler():
 
-    print("Sampler done!")
+        nwalkers = 32
+        ndim = 5
 
-    return sampler
+        # Init starting point for all walkers
+        pos = self.theta_0 + 1e-4 * np.random.randn(nwalkers, ndim)
+
+        # Init emcee EnsembleSampler
+        sampler = emcee.EnsembleSampler(nwalkers, ndim, log_probability)
+
+        # Run the sampler
+        sampler.run_mcmc(pos, 5000, progress=True);
+
+        print("Sampler done!")
+
+        return sampler
