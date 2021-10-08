826c834dfb72ad4013e3270f3dab5585750290e5
SvPodergit config --global user.email Sven.Poder@live.com <Sven.Poder@LIVE.COM>
2021-10-08 16:52:53 +0300

    unittests

---
+++ gaia_tools/test_main.py
@@ -0,0 +1,26 @@
+import unittest
+
+from gaia_tools.data_analysis import*
+from gaia_tools.import_functions import *
+
+
+class gaiaTests(unittest.TestCase):
+    """Tests for GeekTechStuff Grafana API Python"""
+
+    def test_source_id_is_int(self):
+
+        data_icrs = import_data("spectroscopic_test_table.csv")
+        
+        galcen_data = get_transformed_data(data_icrs, 
+                                            include_cylindrical = True,
+                                            debug = True,
+                                            is_bayes = False, 
+                                            is_source_included = True)
+
+        self.assertIs(galcen_data.source_id.iloc[0], int)
+
+
+    
+    
+if __name__ == '__main__':
+    unittest.main()
