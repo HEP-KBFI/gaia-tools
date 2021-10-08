cc3abd66fdf07b010c2b645524bd29eb5595be8a
SvPodergit config --global user.email Sven.Poder@live.com <Sven.Poder@LIVE.COM>
2021-10-08 17:12:38 +0300

    commit

--- gaia_tools/test_main.py
+++ gaia_tools/test_main.py
@@ -9,13 +9,14 @@ class gaiaTests(unittest.TestCase):
 
     def test_source_id_is_int(self):
 
+        print("Echo")
         #data_icrs = import_data("spectroscopic_test_table.csv")
         
-        #galcen_data = get_transformed_data(data_icrs, 
-                                            include_cylindrical = True,
-                                            debug = True,
-                                            is_bayes = False, 
-                                            is_source_included = True)
+        # galcen_data = get_transformed_data(data_icrs, 
+        #                                     include_cylindrical = True,
+        #                                     debug = True,
+        #                                     is_bayes = False, 
+        #                                     is_source_included = True)
 
         #self.assertIs(galcen_data.source_id.iloc[0], int)
 
