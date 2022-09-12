87716230840083e6577de818cab085baa5cff79b
Sven Põder <sven@manivald.local>
2022-09-12 10:16:13 +0300

    gaia dr3

--- scripts/launch_query.sh
+++ scripts/launch_query.sh
@@ -8,4 +8,5 @@ set -e
 # python3 scripts/starhorse_query.py --out /scratch/sven/gaia_downloads/crossmatched_rv_tmass_data.csv
 
 
-python3 scripts/starhorse_query.py --out /home/sven/repos/gaia-tools/test_heidelberg.csv
+python3 scripts/gaia_query.py --out /scratch/sven/gaia_downloads/gaia_dr3_xm_2MASS_photometry_orig_ext_id.csv \
+                                --login
