c851709a4b06469becafefa112a1bfc27f8bfd57
Sven Põder <sven.poder@live.com>
2023-02-20 13:29:21 +0200

    cleanup

--- scripts/velocity_curve_plots.py
+++ scripts/velocity_curve_plots.py
@@ -28,27 +28,22 @@ other_curves_dict = {'Ablimit et al. (2020) - \nproper motion method' : cepheids
                     'Wang et al. (2022)' : vc_data_wang,
                     'Eilers et al. (2018)' : eilers_rc}
 
-# Rotation Curve Plotting Function
-def plot_rotation_curve(is_fit_curve, 
-                        mcmc_runs, 
+def plot_rotation_curve(mcmc_runs, 
                         labels, 
                         burnin, 
                         extra_dims=[], 
                         display_eilers=False, 
-                        display_bin_edges=False, 
-                        is_h_fitted = True):
+                        display_bin_edges=False):
 
-    def func(x, a, b, c, ):
-        return a*x*x + b*x + c
 
     fig, ax = plt.subplots(figsize=(12, 6))
     colors = ['black', 'orange', 'blue', 'green', 'red']
     
-    for i_path, path in enumerate(mcmc_runs):
+    bin_setup = load_bin_settings(mcmc_runs[0], is_print=False)
+    bin_centers_r = bin_setup['bin_centers_r']
 
-        
-        bin_setup = load_bin_settings(path, is_print=False)
-        bin_centers_r = bin_setup['bin_centers_r']
+    mcmc_fmts = ['o', 'd']
+    for i_path, path in enumerate(mcmc_runs):
 
         path = path + '/sampler_' + path.split('/')[-1].split('_')[0] + '.h5'
         reader = emcee.backends.HDFBackend(path, read_only=True)
@@ -76,7 +71,7 @@ def plot_rotation_curve(is_fit_curve,
             x_offset = 0
             x_values = bin_centers_r + x_offset
         else:
-            x_offset = np.linspace(-200, 200, len(mcmc_runs))
+            x_offset = np.linspace(-100, 100, len(mcmc_runs))
             x_values = bin_centers_r + x_offset[i_path]
         y_values = velocity_array[:,0]
         y_error = [velocity_array[:,1], velocity_array[:,2]]
@@ -86,15 +81,9 @@ def plot_rotation_curve(is_fit_curve,
 
         plt.errorbar(x_values, -y_values,
                     yerr= y_error,
-                    fmt='o', c=colors[i_path], label=labels[i_path])
+                    fmt=mcmc_fmts[i_path], c=colors[i_path], label=labels[i_path])
 
-        if(is_fit_curve):
-            popt, pcov = curve_fit(func, bin_centers_r, -y_values, sigma=velocity_array[:,1])
-            perr = np.sqrt(np.diag(pcov))
-            fit = func(np.array(bin_centers_r), popt[0], popt[1], popt[2])
-            plot(bin_centers_r, fit, colors[i_path], lw=2, linestyle="-", alpha=0.75, label='Fitted curve')
-
-    plt.xlabel(r'$r$ [kpc]', fontdict={'fontsize' : 16})
+    plt.xlabel(r'$R$ [kpc]', fontdict={'fontsize' : 16})
     plt.ylabel(r'$v_c$ [km/s]',  fontdict={'fontsize' : 16})
     plt.xticks(fontsize=14, rotation=0)
     plt.yticks(fontsize=14, rotation=0)
@@ -109,17 +98,12 @@ def plot_rotation_curve(is_fit_curve,
     bin_edges_z = bin_setup['bin_edges'][1].T[0]
 
     if(display_bin_edges):
-        # y_min = np.min(y_values)
-        # y_max = np.max(y_values)
-
-        ax.vlines(bin_edges_r, 205, 250, linestyles='dashed', alpha=0.5, colors='grey')
-        
+        ax.vlines(bin_edges_r, 205, 255, linestyles='dashed', alpha=0.5, colors='grey')   
         x_ticks = np.round(bin_edges_r)
-        #x_ticks = np.insert(x_ticks, 0, 4300)
-        #x_ticks = np.insert(x_ticks, -1, 12700)
         ax.set_xticks(x_ticks)
 
-
+    lit_fmts = ['+', 'x', '^', 'v']
+    lit_colors = ['blue', 'green', 'brown', 'red']
     if(display_eilers):
         for i, key in enumerate(other_curves_dict.keys()):
             rc_data = other_curves_dict[key]
@@ -128,22 +112,24 @@ def plot_rotation_curve(is_fit_curve,
             rc_y_values = np.array(rc_data.v_c)
             rc_x_values = np.array(rc_data.R*1e3)
             
-            if(key == 'Eilers et al. (2018)'):
+            if(key == 'Eilers et al. 2018'):
                 rc_y_error = [np.array(rc_data.sig_below), np.array(rc_data.sig_above)]
             else:
                 rc_y_error = [np.array(rc_data.sig), np.array(rc_data.sig)]
 
             plt.errorbar(rc_x_values, rc_y_values, 
                     yerr=rc_y_error, 
-                    fmt='v',
+                    fmt=lit_fmts[i],
+                    color = lit_colors[i],
                     label=key)
-    plt.legend(prop={'size': 7})
- 
-    r_range = r', $r \in [{},{}]$ kpc'.format(np.round(bin_edges_r[0]/1000), np.round(bin_edges_r[-1]/1000))
-    z_range = r', $z \in [{},{}]$ pc'.format(np.round(bin_edges_z[0]), np.round(bin_edges_z[-1]))
-    #plt.title('Rotation Curve' + r_range + z_range, fontsize=18, pad=12)
 
+    plt.legend(prop={'size': 9})
+
+    h_lines  = np.arange(210, 251, 10)
+    ax.hlines(h_lines, 4500, 15500, linestyles='dashed', alpha=0.5, colors='grey')
+     
     plt.xlim(4000, 16000)
+    plt.rcParams["axes.grid"] = False
 
 def load_bin_settings(folder_path, is_print=False):
     with open(folder_path + '/run_settings.txt', 'rb') as handle:
